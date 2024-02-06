0b4f956c1a33a08049c1b0e47c11ae5dc33c3bb0
OlegSvitlichniy <gagaagaga0@gmail.com>
2024-02-06 20:46:00 +0200

    Home task

--- src/HomeWorkJAVA/HW2AQA.java
+++ src/HomeWorkJAVA/HW2AQA.java
@@ -4,13 +4,12 @@ public class HW2AQA {
     public static void main(String[] args) {
         /*TASk1*/
         String string1 = "This line that i want to cut, cause it is too long";
-        String string2 = "This line that i want to cut, cause it is too long".substring(0 , 35);
+        String string2 = string1.substring(0 , 35);
         String string3 = "This line that don't want to cut, cause it is perfect".substring(0 , 35);
-
-
         System.out.println(string1 + " = " + string1.length());
         System.out.println(string2 + " = " + string2.length());
         System.out.println(string3 + " = " + string3.length());
+        System.out.println("================================================================================");
 
         /*TASK2*/
         String jobtextTask2 ="Testing is my favourite job";
@@ -44,7 +43,7 @@ public class HW2AQA {
         /*TASK3*/
         String text3 = "Completely random text in English. "
                 + "In it, we just need to determine how many times the character 'a' occurs there. "
-                + "And we can use the split method and the length method.";
+                + "And we can use the split method and the length method.".toLowerCase();
         String[] splittext3 = text3.split("a");
         int splitT3 = splittext3.length-1;
         System.out.println(splitT3);
